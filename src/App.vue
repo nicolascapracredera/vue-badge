a2e9fc4ff747976d56114501b4bba4c12df0485d
Nicolas Capra <nicolas.capra@credera.com>
2020-09-29 11:33:15 -0600

    Completed form styling

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="/">Marvel Character Lookup</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav">
-          <li :class="'nav-item ' + [currentPage === '/' && 'active']">
-            <router-link class="nav-link" to="/">Home</router-link>
-          </li>
-          <li :class="'nav-item ' + [currentPage === '/search' && 'active']">
-            <router-link class="nav-link" to="/search">Search for Characters</router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <div id="content">
-      <router-view></router-view>
-      <div id="footer">
-        <a href="http://marvel.com">Data provided by Marvel. © 2020 MARVEL</a>
+    <div id="page">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">Marvel Character Lookup</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav">
+            <li :class="'nav-item ' + [currentPage === '/' && 'active']">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li :class="'nav-item ' + [currentPage === '/search' && 'active']">
+              <router-link class="nav-link" to="/search">Search for Characters</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div id="content">
+        <router-view></router-view>
       </div>
     </div>
+    <div id="footer">
+          <a href="http://marvel.com">Data provided by Marvel. © 2020 MARVEL</a>
+      </div>
   </div>
 </template>
 
@@ -40,11 +42,18 @@ export default {
 
 <style scoped>
 #footer {
-  clear: both;
-  margin-top: 4%;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 2.5rem;
 }
 
 #content {
   margin-left: 5px;
+}
+
+#page {
+  position: relative;
+  min-height: 100vh;
 }
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,60 +6,73 @@
         save your searches and keep lists for your personal use</p>
     <div>
       <form
-  id="app"
-  @submit="checkForm"
-  action="https://vuejs.org/"
-  method="post"
+  id="form"
 >
-
-  <p v-if="errors.length">
-    <b>Please correct the following error(s):</b>
-    <ul>
-      <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-    </ul>
-  </p>
-
-  <p>
+  <div class="form-group col-4" style="float:left;">
     <label for="name">Name</label>
     <input
       id="name"
       v-model="name"
       type="text"
       name="name"
+      class="form-control"
+      required
     >
-  </p>
+    <div class="valid-feedback">
+        Looks good!
+      </div>
+    <div class="invalid-feedback">
+      Your name is required
+    </div>
+  </div>
 
-  <p>
-    <label for="age">Age</label>
+  <div class="form-group col-4" style="float:left;">
+    <label for="email">Email</label>
+    <input
+      id="email"
+      v-model="email"
+      type="text"
+      name="email"
+      min="0"
+      class="form-control"
+      required
+    >
+    <div class="valid-feedback">
+      Looks good!
+    </div>
+    <div class="invalid-feedback">
+      {{ emailError }}
+    </div>
+  </div>
+
+  <div class="form-group col-4" style="float:left;">
+    <label for="age">Age {{age}}</label>
     <input
       id="age"
       v-model="age"
-      type="number"
+      type="range"
+      min="1"
+      max="99"
       name="age"
-      min="0"
+      class="custom-range"
+      required
     >
-  </p>
+    <div class="valid-feedback">
+      Looks good!
+    </div>
+    <div class="invalid-feedback">
+      {{ ageError }}
+    </div>
+  </div>
 
-  <p>
-    <label for="movie">Favorite Movie</label>
-    <select
-      id="movie"
-      v-model="movie"
-      name="movie"
-    >
-      <option>Star Wars</option>
-      <option>Vanilla Sky</option>
-      <option>Atomic Blonde</option>
-    </select>
-  </p>
-
-  <p>
-    <input
+  <div class="form-group" id="button-container">
+    <button
+      style="margin-left:50%;"
       type="submit"
-      value="Submit"
-    >
-  </p>
-
+      class="btn btn-primary">
+      Submit
+    </button>
+  </div>
 </form>
     </div>
   </div>
@@ -71,33 +84,63 @@ export default {
   name: 'Home',
   data: function() { 
     return {
-      errors: [],
       name: null,
-      age: null,
-      movie: null
+      email: null,
+      age: 1,
+      emailError: "",
+      ageError: ""
     }
   },
   methods:{
-    checkForm: function (e) {
-      if (this.name && this.age) {
-        return true;
-      }
+    checkEmail: function () {
+      const emailRequiredError = 'Email required.';
+      const emailFormatError = 'Email is not in the correct format.';
+      const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 
-      this.errors = [];
+      document.getElementById("email").classList.remove("is-invalid");
+      document.getElementById("email").classList.remove("is-valid");
 
-      if (!this.name) {
-        this.errors.push('Name required.');
+      if (!this.email) {
+        this.emailError = emailRequiredError;
+        document.getElementById("email").classList.add("is-invalid");
+      } else if (!emailRegex.test(this.email)) {
+        this.emailError = emailFormatError;
+        document.getElementById("email").classList.add("is-invalid");
+      } else {
+        document.getElementById("email").classList.add("is-valid");
       }
-      if (!this.age) {
-        this.errors.push('Age required.');
-      }
+    },
+    checkAge: function() {
+      const minAgeError = 'You need to be 18 or older to use this service.';
 
-      e.preventDefault();
+      document.getElementById("age").classList.remove("is-invalid");
+      document.getElementById("age").classList.remove("is-valid");
+
+      if (this.age < 18) {
+        this.ageError = minAgeError;
+        document.getElementById("age").classList.add("is-invalid");
+      } else {
+        document.getElementById("age").classList.add("is-valid");
+      }
+    }
+  },
+  watch: {
+    email: function() {
+      this.checkEmail();
+    },
+    age: function() {
+      this.checkAge();
+    }, 
+    name: function() {
+      document.getElementById("name").parentElement.classList.add("was-validated");
     }
   }
 }
 </script>
 
 <style scoped>
-
+#button-container {
+  float: left;
+  width: 100%;
+}
 </style>
